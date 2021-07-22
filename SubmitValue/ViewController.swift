9e4667242b887edf3b5aa20902319a994d688775
SungKihun <tjdrlgns3293@naver.com>
2021-07-23 00:56:45 +0900

    Initial Commit

---
+++ SubmitValue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SubmitValue
+//
+//  Created by 성기훈 on 2021/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
